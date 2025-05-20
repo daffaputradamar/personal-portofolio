48c4d1612a123ce990a8e90d35d8ed48b83dc34e
Daffa Akbar <daffaputradamar@gmail.com>
2025-05-20 10:09:32 +0700

    refactor: reorganize Home component structure and update dev server port

--- app/page.tsx
+++ app/page.tsx
@@ -3,26 +3,22 @@ import Intro from '@/components/intro'
 import RecentPosts from '@/components/recent-posts'
 import RecentProjects from '@/components/recent-projects'
 import { BackgroundBeams } from '@/components/ui/background-beams'
-import { ShootingStars } from '@/components/ui/shooting-stars'
-import { StarsBackground } from '@/components/ui/stars-background'
 import { TracingBeam } from '@/components/ui/tracing-beams'
 
 export default function Home() {
   return (
     <section className='pb-24 pt-40'>
+      <BackgroundBeams />
+
       <TracingBeam className='px-6'>
-
-      <ShootingStars />
-      <StarsBackground />
-
-      <div className='container max-w-3xl'>
-        <Intro />
-        <Experiences/>
-        <RecentPosts />
-        <RecentProjects />
-      </div>
-
+        <div className='container max-w-3xl'>
+          <Intro />
+          <Experiences />
+          <RecentPosts />
+          <RecentProjects />
+        </div>
       </TracingBeam>
+
     </section>
   )
 }
